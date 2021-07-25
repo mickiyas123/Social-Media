a7a9ab046fad01edea4ae33a64043991879807a7
mickiy123 <mikikinfe345@gmail.com>
2021-07-25 22:47:57 +0300

    Sign Up view

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import forms
 from django.shortcuts import render
+# importing generic views
+from django.views.generic import (CreateView)
+# for redirecting once us loggedin or loggedout
+from django.urls import reverse_lazy
+# importing forms
+from . import forms
 
 # Create your views here.
+
+# new user signing up 
+class SignUp(CreateView):
+    # connecting to te form
+    form_class = forms.UserCreateForm
+    # redirecting to login page after signup
+    success_url = 'login'
+    # connectin to template 
+    template_name = 'accounts/signup.html'
