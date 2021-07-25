0851ae0b3c6ea9539fa0d90db4c0be9848a6bc48
mickiy123 <mikikinfe345@gmail.com>
2021-07-25 22:29:09 +0300

    model for users

--- simplesocial/accounts/models.py
+++ simplesocial/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# importing reverse lazy for redirecting
+from django.urls import reverse_lazy
+# importing auth 
+from django.contrib  import auth
 
 # Create your models here.
+
+# model for users
+class Users(auth.models.User):
+     
+    # a string representation of the object 
+    def __str__(self):
+        return "@{}".format(self.username)
