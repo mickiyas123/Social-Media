7c2a96e9063743393ac3cacb07a2ba3c431ca876
mickiy123 <mikikinfe345@gmail.com>
2021-07-26 12:21:08 +0300

    a test and Thank you view

--- simplesocial/simplesocial/views.py
+++ simplesocial/simplesocial/views.py
@@ -1,6 +1,15 @@
 # Importing Generic View
 from django.views.generic import (TemplateView)
 
+
+# a view for accessing test.html
+class Test(TemplateView):
+    template_name = 'test.html'
+
+# a view for accessing thanks.html
+class ThankYou(TemplateView):
+    template_name = 'thanks.html'
+
 # a view for Home page
 class HomePage(TemplateView):
     # accessing the index html since it's the homepage
