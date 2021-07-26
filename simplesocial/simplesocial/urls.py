162ae5030f4c8258fd03808ca93c45bdcee88d43
mickiy123 <mikikinfe345@gmail.com>
2021-07-26 11:02:43 +0300

    a path for connecting the webapp and project

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 # importing views we created
 from . import views
 
@@ -22,4 +22,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # a path for accessing home page view
     path('',views.HomePage.as_view(),name='home'),
+    # a path for connecting the project and webapp
+    path('accounts',include('accounts.urls',namespace="accounts")),
+    # a path for authorization purpose
+    path('accounts',include('django.contrib.auth.urls')),
+
 ]
