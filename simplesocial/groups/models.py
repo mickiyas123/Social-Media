4f4a00cfb474e0e42cfb07c70874112b80a19b91
mickiy123 <mikikinfe345@gmail.com>
2021-07-27 09:45:08 +0300

    imported necessary modules

--- simplesocial/groups/models.py
+++ simplesocial/groups/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+# to replace space's in url with -
+from django.utils.text import slugify
+# for link embeding
+import misaka
+# getting the current active user
+from django.contrib.auth import get_user_model
+# user object
+User = get_user_model()
 
-# Create your models here.
+# using custom template tags
+from django import template
+register = template.Library()
+
+
+
+
