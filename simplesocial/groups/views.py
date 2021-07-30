af5a1dbfff4b426317d4bfef5ae9d933fda5385d
mickiy123 <mikikinfe345@gmail.com>
2021-07-30 12:24:35 +0300

    Imported the neccessary modules

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+# importing permission mixins
+from django.contrib.auth.mixins import (LoginRequiredMixin,PermissionRequiredMixin)
+# importing reverse for redirecting to different pages
+from django.urls import reverse
+# importing generic views
+from django.views import generic
+# importing models of the group webapp
+from groups.models import Group,GroupMember
 
 # Create your views here.
+
+# class view for creating Posts 
+# class CreatePost(LoginRequiredMixin,generic.CreateView):
+#     fields = ('name','description')
+#     model = 
+
