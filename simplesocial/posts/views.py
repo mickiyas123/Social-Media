157a9933bc7c441ca19f33676f99921e359db048
mickiy123 <mikikinfe345@gmail.com>
2021-07-30 16:09:47 +0300

    Imported the necessary modules

--- simplesocial/posts/views.py
+++ simplesocial/posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+# importing mixins to check login
+from django.contrib.auth.mixins import LoginRequiredMixin
+# redirecting to html pages
+from django.urls import reverse_lazy
+# imprting views and HttpResponse
+from django.http import Http404
+from django.views import generic
+# third library mixin
+from braces.views import SelectRelatedMixin 
+# importing models and forms
+from . import models
+from . import forms
+# importing user model
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 # Create your views here.
