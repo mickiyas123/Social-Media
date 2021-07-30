bedae7c7e617e2036b502340d4dea61f686f6d05
mickiy123 <mikikinfe345@gmail.com>
2021-07-30 15:17:45 +0300

    Imported necessary modules and assign app name

---
+++ simplesocial/groups/urls.py
@@ -0,0 +1,9 @@
+# GROUPS URL FILE
+# importing the necessary modules
+from django.conf.urls import url
+from django.urls import path,include
+from groups import views
+
+# app name for dynamic url
+app_name = 'groups'
+
