23b3b34d8d669eff9e135a560babdb145453c4bc
mickiy123 <mikikinfe345@gmail.com>
2021-07-30 15:37:30 +0300

    registered the group model

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,11 @@
+from groups.models import Group
 from django.contrib import admin
+# importing the models
+from . import models
 
 # Register your models here.
+
+
+
+# registering the group model
+admin.site.register(models.Group)
