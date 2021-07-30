fdceca607a32b4deef83d504f85b70f617821788
mickiy123 <mikikinfe345@gmail.com>
2021-07-30 15:39:56 +0300

    used TabularInlne to register GroupMember model

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,10 +1,13 @@
-from groups.models import Group
+from groups.models import Group, GroupMember
 from django.contrib import admin
 # importing the models
 from . import models
 
 # Register your models here.
 
+# i can see the group members when i click the group an i can edit the group members
+class GroupMemberInline(admin.TabularInline):
+    model = GroupMember
 
 
 # registering the group model
