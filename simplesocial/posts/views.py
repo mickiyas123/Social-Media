904b29884aa74305ed2b7d619ed6d452dbe34f55
mickiy123 <mikikinfe345@gmail.com>
2021-07-31 10:16:20 +0300

    A view for creating and deleting a post

--- simplesocial/posts/views.py
+++ simplesocial/posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.signals import user_logged_in
+from django.core.checks import messages
 from django.db.models import query
 from django.shortcuts import render
 # importing mixins to check login
@@ -37,6 +38,7 @@ class UserPosts(generic.ListView):
     # checking if user exist and get posts related to that specific user
     def get_queryset(self):
         try:
+            # getting the exact posts related to the current loggen in user
             self.post.user = User.objects.prefetch_related('posts').get(username__iexact = self.kwargs.get('username'))
         except User.DoesNotExist:
             raise Http404
@@ -59,4 +61,31 @@ class PostDetail(SelectRelatedMixin,generic.DetailView):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(user__username__iexact = self.kwargs.get('username'))
+
+# Creating Post
+class CreatePost(LoginRequiredMixin,SelectRelatedMixin,generic.CreateView):
+    fields = ('message','group')
+    model = models.Post
+
+    # checking if the form is valid  
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+# Delete Post
+
+class DeletePost(LoginRequiredMixin,SelectRelatedMixin,generic.DeleteView):
+    model = models.Post
+    select_related = ('user','group')
+    success_url = reverse_lazy('posts:all')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user_id = self.user.request.user.id)
+
+    def delete(self,*args,**kwargs):
+        messages.success(self.request,'Post Deleted')
+        return super().delete(*args,**kwargs)    
 
