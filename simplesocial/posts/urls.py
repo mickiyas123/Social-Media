3914d93e7f20d1f3e57e309a904d90088a3a1fae
mickiy123 <mikikinfe345@gmail.com>
2021-07-31 10:54:00 +0300

    Created a path for list of post,create post,delete post and others

--- simplesocial/posts/urls.py
+++ simplesocial/posts/urls.py
@@ -3,5 +3,20 @@ from django.urls import path
 
 from . import views
 
+# for dynamical url
 app_name = 'posts'
+
+urlpatterns = [
+    # url to see list of the post
+    path('',views.PostList.as_view(),name='all'),
+    # path for creating a new post
+    path('new/',views.CreatePost.as_view(),name='create'),
+    # list of user post
+    url('by/(?P<username>[-\w]+)/$',views.UserPosts.as_view(),name='for_user'),
+    # pat to single post
+    url('by/(?P<username>[-\w]+)/(?P<pk>\d+)/$',views.PostDetail.as_view(),name='single'),
+    # path for deleting a post
+    url('delete/(?P<pk>\d+)/$',views.DeletePost.as_view(),name='delete')
+
+]
 
